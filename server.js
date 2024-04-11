ef4ab4a4475787d174b69e1ace454e1ce5d84f39
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-04-11 00:15:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,16 @@ require('dotenv').config();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 const get_ = require('./services');
-app.use(cors());
+app.use(cors()); 
 
-// Middleware para manejar la solicitud GET de /book/:id/:slug
-app.get('/book/:id/:slug', async (req, res, next) => {
-  const id = req.params.id;
-  const slug = req.params.slug;
-  res.render('book-read', { title: 'StartSyncX', bookId: id, slug: slug });
+app.get('/', async (req, res) => {
+  res.render('index', { title: 'StartSyncX', url_api_category:'ss' });
 });
 
-// Middleware para manejar todas las demás solicitudes GET
-app.get('*', async (req, res) => {
-  res.render('index', { title: 'StartSyncX', url_api_category: 'ss' });
+app.get('/book/:id/:slug/?loc=:name', async (req, res) => {
+  const id = req.params.id; 
+  const slug = req.params.slug; 
+  res.render('book-read', { title: 'StartSyncX', bookId: id, slug:slug });
 });
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
