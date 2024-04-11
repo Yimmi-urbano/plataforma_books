808ff614db76b647dcfe19effe9d35cc27234c40
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-04-10 23:39:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,26 +9,17 @@ app.set('views', __dirname + '/views');
 const get_ = require('./services');
 app.use(cors()); 
 
-// Ruta para servir la página HTML de inicio
 app.get('/', async (req, res) => {
   res.render('index', { title: 'StartSyncX', url_api_category:'ss' });
 });
 
-// Ruta para servir la página de lectura de libros
 app.get('/book/:id/:slug', async (req, res) => {
   const id = req.params.id; 
   const slug = req.params.slug; 
-  res.render('book-read', { title: 'StartSyncX', bookId: id, slug: slug });
+  res.render('book-read', { title: 'StartSyncX', bookId: id, slug:slug });
 });
 
-// Ruta para servir archivos estáticos (CSS, imágenes, etc.)
 app.use('/public', express.static(path.join(__dirname, 'public')));
-
-// Middleware para redirigir todas las demás solicitudes a la página principal
-app.use((req, res) => {
-  res.redirect('/');
-});
-
 app.listen(get_.PORT, () => {
   console.log(`Servidor escuchando en el puerto ${get_.PORT}`);
 });
