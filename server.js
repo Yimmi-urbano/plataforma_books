5cc3338ed835ff4127493890e55c0e41d208ac21
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-04-11 00:09:01 -0500

    slug

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ require('dotenv').config();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 const get_ = require('./services');
-app.use(cors()); 
+app.use(cors());
 
-app.get('/', async (req, res) => {
-  res.render('index', { title: 'StartSyncX', url_api_category:'ss' });
+// Middleware para manejar la solicitud GET de /book/:id/:slug
+app.get('/book/:id/:slug', async (req, res, next) => {
+  const id = req.params.id;
+  const slug = req.params.slug;
+  res.render('book-read', { title: 'StartSyncX', bookId: id, slug: slug });
 });
 
-app.get('/book/:id/:slug', async (req, res) => {
-  const id = req.params.id; 
-  const slug = req.params.slug; 
-  res.render('book-read', { title: 'StartSyncX', bookId: id, slug:slug });
+// Middleware para manejar todas las demás solicitudes GET
+app.get('*', async (req, res) => {
+  res.render('index', { title: 'StartSyncX', url_api_category: 'ss' });
 });
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
